391a11315bf359d6a1b0ee187328fbbcb44fc0b6
The-Nico26 <nicolas.mazard@viacesi.fr>
2017-04-06 15:48:50 +0200

    First

---
+++ Pages/navigation.php
@@ -0,0 +1,15 @@
+<div class="container"></div>
+<nav>
+	<div class="link">
+		<a href="#"><div>Acceuil</div></a>
+		<a href="#"><div>La Vie Associative</div></a>
+		<a href="#"><div>Partenaires</div></a>
+		<a href="#"><div>Evenements</div></a>
+		<span class="logo"><img src="../Images/logo.png" height="100px"></span>
+		<a href="#"><div>Albums Photos</div></a>
+		<a href="#"><div>Point de vente</div></a>
+		<a href="#"><div>Forum</div></a>
+		<a href="#"><div>Votre BDE</div></a>
+		<a class="connect" href="#"><div><i class="fa fa-user"></i></div></a>
+	</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+		<link rel="stylesheet" type="text/css" href="Styles/style.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<title>BDE du Cesi de Lyon</title>
+	</head>
+	<body>
+		<div class="container"></div>
+		<div class="panel-left">
+			<div class="content">
+				<div class="form">
+					Pseudo : <br>
+					<input type="text" name="name" placeholder="Pseudo">
+					Mot de passe : <br>
+					<input type="password" name="password" placeholder="Password">
+					<button>Connexion</button>
+				</div>
+			</div>
+		</div>
+		<nav>
+			<div class="link">
+				<a href="#"><div>Acceuil</div></a>
+				<a href="#"><div>La Vie Associative</div></a>
+				<a href="#"><div>Partenaires</div></a>
+				<a href="#"><div>Evenements</div></a>
+				<span class="logo"><img src="../Images/logo.png" height="100px"></span>
+				<a href="#"><div>Albums Photos</div></a>
+				<a href="#"><div>Point de vente</div></a>
+				<a href="#"><div>Forum</div></a>
+				<a href="#"><div>Votre BDE</div></a>
+				<a class="connect" href="#"><div><i class="fa fa-user"></i></div></a>
+			</div>
+		</nav>
+			<div class="event">
+				<div class="title">
+					Evènement prochain :
+				</div>
+				<div class="event_pro">test</div>
+				<div class="navigation">
+					<i class="fa fa-arrow-left"></i> <i class="fa fa-arrow-right"></i>
+				</div>
+			</div>
+		<div class="body">
+
+			<section>
+				<article class="actualite">
+					<div class="title">
+						<div class="title_ext_left"></div>
+						<div class="title_left"></div>
+						<div class="title_center">
+							<span class="title_text">Actualité</span>
+							<span class="title_right">
+								<span class="title_ext_right"></span>
+							</span>
+						</div>
+					</div>
+
+					<div class="news">
+						<div class="triangle">
+							<div class="triangle_left"></div>
+							<div class="triangle_center">
+								<span class="title">Coucou</span>
+								<span class="triangle_right"></span>
+							</div>
+						</div>
+						<div class="new">
+							<span class="date">Le 25 janvier 2018</span>
+							<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						</div>
+
+						<div class="triangle">
+							<div class="triangle_left"></div>
+							<div class="triangle_center">
+								<span class="title">TitreTitreTitreTitre</span>
+								<span class="triangle_right"></span>
+							</div>
+						</div>
+						<div class="new">
+							<span class="date">Le 25 janvier 2018</span>
+							<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+						</div>
+					</div>
+					<div class="navigation">
+						<button><</button>
+						<button>1</button>
+						<button>2</button>
+						<button>></button>
+					</div>
+				</article>
+				<article class="network_social">
+
+					<div class="title">
+						<div class="title_ext_left"></div>
+						<div class="title_left"></div>
+						<div class="title_center">
+							<span class="title_text">Facebook</span>
+							<span class="title_right">
+								<span class="title_ext_right"></span>
+							</span>
+						</div>
+					</div>
+					<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBDECesiLyon%2F&amp;tabs=timeline&amp;width=340&amp;height=500&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=false&amp;appId" scrolling="yes" allowtransparency="true" frameborder="0"></iframe>
+				</article>
+			</section>
+
+			<footer>
+				Copyright © - 2017 - BDE Cesi.Lyon - All right Reserved
+			</footer>
+		</div>
+		<script type="text/javascript" src="http://livejs.com/live.js"></script>
+	</body>
+</html>
